8bcb9b096e4714c1e3be4793e4a39c75bcbdda4a
Ashish90087 <ashish.sharma90087@gmail.com>
2022-04-20 15:55:25 +0530

    some changes done

--- rms_copy/rmsui/src/app/welcome-admin/welcome-admin.component.ts
+++ rms_copy/rmsui/src/app/welcome-admin/welcome-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { CommonService } from '../services/common.service';
 @Component({
   selector: 'app-welcome-admin',
   templateUrl: './welcome-admin.component.html',
@@ -7,14 +8,42 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class WelcomeAdminComponent implements OnInit {
 
-  constructor( private authservice: AuthService,) { }
+  constructor( private authservice: AuthService,private cms : CommonService) { }
   public user_id: any;
   public user_name: any;
+  public app_count :any;
+  public emp_count :any;
+  public server_count :any;
+
+  getAppCount() {
+    this.cms.getFunction('appcount').subscribe((res:any) => {
+      console.log(res);
+      this.app_count= res[0].x;
+    });
+  }
+
+  getEmpCount() {
+    this.cms.getFunction('empcount').subscribe((res:any) => {
+      console.log(res);
+      this.emp_count= res[0].x;
+    });
+  }
+
+  getServerCount() {
+    this.cms.getFunction('servercount').subscribe((res:any) => {
+      console.log(res);
+      this.server_count= res[0].x;
+    });
+  }
+
   ngOnInit(): void {
     let user = this.authservice.currentUser;
     this.user_id = user.userid;
     this.user_name= user.user_name;
     console.log(user.user_name);
+    this.getAppCount();
+    this.getEmpCount();
+    this.getServerCount();
 
   }
 }
