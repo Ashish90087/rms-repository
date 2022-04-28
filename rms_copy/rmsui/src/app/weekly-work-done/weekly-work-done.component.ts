13139afb5d5308a7970ee422b0fe9a951da16fd4
Ashish90087 <ashish.sharma90087@gmail.com>
2022-04-28 15:33:50 -0700

    chnges done 28 april

--- rms_copy/rmsui/src/app/weekly-work-done/weekly-work-done.component.ts
+++ rms_copy/rmsui/src/app/weekly-work-done/weekly-work-done.component.ts
@@ -9,6 +9,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-weekly-work-done',
@@ -28,7 +29,7 @@ export class WeeklyWorkDoneComponent implements OnInit {
   })
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
   @ViewChild('scroll', {read : ElementRef}) public scroll!: ElementRef<any>;
-  displayedColumns: string[] = ['sn', 'week','app_name', 'work_done','action'];
+  displayedColumns: string[] = ['sn', 'week','app_name', 'work_done'];
   constructor(private fB : FormBuilder,private cms : CommonService,private authservice: AuthService, private datePipe : DatePipe) { }
   public task_data: any = [];
   dataSource: any = [];
@@ -62,25 +63,31 @@ export class WeeklyWorkDoneComponent implements OnInit {
 
 
 
-  // dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
-  //   //console.log(dateRangeStart.value);
-  //   this.temp1 =dateRangeStart.value;
-  //  // console.log(dateRangeEnd.value);
-  //   this.temp2 =dateRangeEnd.value;
-  //   //this.taskForm.value.week =this.temp1+'-'+this.temp2;
-  //   this.taskForm.value.week.start=this.temp1;
-  //   this.taskForm.value.week.end=this.temp2;
-  //   //console.log(this.taskForm.value);
-  //   this.taskForm.value.week = this.taskForm.value.week.start+'-'+this.taskForm.value.week.end
-  // }
+  dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
+    console.log(dateRangeStart.value);
+    this.temp1 =dateRangeStart.value;
+    let momentVariable = moment(this.temp1, 'MM-DD-YYYY');
+    this.taskForm.value.week.start = momentVariable.format('YYYY-MM-DD');
+    console.log(this.taskForm.value.week.start);
+
+    console.log(dateRangeEnd.value);
+    this.temp2 =dateRangeEnd.value;
+    let moment2Variable = moment(this.temp2, 'MM-DD-YYYY');
+    this.taskForm.value.week.end = moment2Variable.format('YYYY-MM-DD');
+    console.log(this.taskForm.value.week.end);
+    //this.taskForm.value.week =this.temp1+'-'+this.temp2;
+    
+    //console.log(this.taskForm.value);
+    //this.taskForm.value.week = this.taskForm.value.week.start+'-'+this.taskForm.value.week.end
+  }
 
 
   onSubmit(){
 
-    this.taskForm.patchValue({
-      start : this.datePipe.transform(this.taskForm.get("start")?.value, "yyyy-MM-dd"),
-      end : this.datePipe.transform(this.taskForm.get("end")?.value, "yyyy-MM-dd"),
-    })
+    // this.taskForm.patchValue({
+    //   start : this.datePipe.transform(this.taskForm.get("start")?.value, "yyyy-MM-dd"),
+    //   end : this.datePipe.transform(this.taskForm.get("end")?.value, "yyyy-MM-dd"),
+    // })
     console.log(this.taskForm.value);
     let d:any=[];
     this.data=this.taskForm.value;
@@ -101,7 +108,7 @@ export class WeeklyWorkDoneComponent implements OnInit {
       "work_done": this.data.work_detail[i].work_done }).subscribe((res:any) => {
       console.log(res);
       if (res['affectedRows']) {
-         //this.getEmpDetails();
+         this.getWorkDetails();
          this.taskForm.reset();
          Swal.fire({ icon: 'success', text: "Saved Successfully.", timer: 2000 });
 
@@ -158,36 +165,38 @@ export class WeeklyWorkDoneComponent implements OnInit {
   }
 
   public temp: any = []
-  onEdit(dept_code:any) {
-    // window.scroll({ top: 0, left: 0});
-  //  this.scroll.nativeElement.scrollTop=this.scroll.nativeElement.scrollHeight;
-   this.scroll.nativeElement.scrollIntoView();
-    // let body={
-    //   dept_id: dept_id,
-    //   user_id:user_id,
-    //   cpu_sno:cpu_sno
-    // }
-    this.x=1;
+  // onEdit(app_id:any) {
+  //   // window.scroll({ top: 0, left: 0});
+  // //  this.scroll.nativeElement.scrollTop=this.scroll.nativeElement.scrollHeight;
+  //  this.scroll.nativeElement.scrollIntoView();
+  //   // let body={
+  //   //   dept_id: dept_id,
+  //   //   user_id:user_id,
+  //   //   cpu_sno:cpu_sno
+  //   // }
+  //   this.x=1;
 
-    //console.log("Which ID is this :" ,this.dataSource.affectedRows);
-    //console.log("Stock Update insert ID is :" ,this.stockForm.value.insertId);
-    this.cms.getFunction('users').subscribe((res: any)=>{
-      //console.log("result of response is res",res);
-      //console.log("res length",res.length);
-      for(let i=0;i<res.length;i++){
-        if(res[i].dept_code==dept_code){
-          console.log("res i is ",res[i]);
-          this.temp=res[i];
-        }
-      }
-      console.log("result of id dept data is",this.temp);
-      this.taskForm.patchValue({
-      dept_code: this.temp.dept_code,
-      dept_name : this.temp.dept_name,
-      remarks:this.temp.remarks
+  //   //console.log("Which ID is this :" ,this.dataSource.affectedRows);
+  //   //console.log("Stock Update insert ID is :" ,this.stockForm.value.insertId);
+  //   this.cms.getFunction('weekly'+"/"+this.taskForm.value.user_id).subscribe((res: any)=>{
+  //     console.log("result of response is res",res);
+  //     //console.log("res length",res.length);
+  //     for(let i=0;i<res.length;i++){
+  //       if(res[i].app_id==app_id){
+  //         console.log("res i is ",res[i]);
+  //         this.temp=res[i];
+  //       }
+  //     }
+  //     console.log("result of id dept data is",this.temp);
+  //     this.taskForm.patchValue({
+  //     app_id: this.temp.app_id,
+  //     app_name:this.temp.app_name,
+  //     work_done:this.temp.work_done
 
-      })
-    });
-  }
+  //     })
+  //   });
+  // }
 
 }
+
+
